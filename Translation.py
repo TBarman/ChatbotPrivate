d8b46ad0a5529f498f3d9eb4aa1529cabde4e604
TBarman <76855370+TBarman@users.noreply.github.com>
2024-07-20 16:47:57 -0400

    Update Translation.py

--- Translation.py
+++ Translation.py
@@ -1,31 +1,26 @@
 import os
-import requests, json
-
+import requests
+from dotenv import load_dotenv
 
 def main():
+
+    load_dotenv()
     global translator_endpoint
     global cog_key
     global cog_region
 
     try:
-        # Get Configuration Settings
-        cog_key = "a9c9b1d7b0714cf2a2310b6c6b99cc50"
-        cog_region = "eastus"
-        translator_endpoint = 'https://api.cognitive.microsofttranslator.com'
+        cog_key = os.getenv('COG_KEY')
+        cog_region = os.getenv('COG_REGION')
+        translator_endpoint = os.getenv('TRANSLATOR_ENDPOINT')
 
-        # Analyze each text file in the reviews folder
         reviews_folder = 'reviews_folder'
         for file_name in os.listdir(reviews_folder):
-            # Read the file contents
             print('\n-------------\n' + file_name)
             text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
             print('\n' + text)
-
-            # Detect the language
             language = GetLanguage(text)
             print('Language:', language)
-
-            # Translate if not already English
             if language != 'en':
                 translation = Translate(text, language)
                 print("\nTranslation:\n{}".format(translation))
@@ -35,13 +30,11 @@ def main():
 
 
 def GetLanguage(text):
-    # Default language is English
     language = 'en'
-    cog_key = "a9c9b1d7b0714cf2a2310b6c6b99cc50"
-    cog_region = "eastus"
-    translator_endpoint = 'https://api.cognitive.microsofttranslator.com'
-    # Use the Translator detect function
-    # Use the Translator detect function
+    cog_key = os.getenv('COG_KEY')
+    cog_region = os.getenv('COG_REGION')
+    translator_endpoint = os.getenv('TRANSLATOR_ENDPOINT')
+    
     path = '/detect'
     url = translator_endpoint + path
 
@@ -60,28 +53,19 @@ def GetLanguage(text):
         'text': text
     }]
 
-    # Send the request and get response
     request = requests.post(url, params=params, headers=headers, json=body)
     response = request.json()
-
-    # Parse JSON array and get language
     language = response[0]["language"]
 
-    # Return the language
     return language
 
 
 def Translate(text, source_language):
-    translation = ''
-    cog_key = "a9c9b1d7b0714cf2a2310b6c6b99cc50"
-    cog_region = "eastus"
-    translator_endpoint = 'https://api.cognitive.microsofttranslator.com'
-    # Use the Translator translate function
-    # Use the Translator translate function
+    cog_key = os.getenv('COG_KEY')
+    cog_region = os.getenv('COG_REGION')
+    translator_endpoint = os.getenv('TRANSLATOR_ENDPOINT')
     path = '/translate'
     url = translator_endpoint + path
-
-    # Build the request
     params = {
         'api-version': '3.0',
         'from': source_language,
@@ -98,14 +82,10 @@ def Translate(text, source_language):
         'text': text
     }]
 
-    # Send the request and get response
     request = requests.post(url, params=params, headers=headers, json=body)
     response = request.json()
-
-    # Parse JSON array and get translation
     translation = response[0]["translations"][0]["text"]
 
-    # Return the translation
     return translation
 
 
