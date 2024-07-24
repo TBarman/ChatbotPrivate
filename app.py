fb21fb935e95e47b37b8d79d811c84c428787acc
TBarman <76855370+TBarman@users.noreply.github.com>
2024-07-23 21:41:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -135,11 +135,27 @@ def main():
         elif st.session_state.messages[-1]["text"][:7] == "QUERY: ":
             # from https://medium.com/@koratarpans99/natural-language-to-sql-with-langchain-nl2sql-f4adc84b81da
 
+
+            # local
             db_user = os.getenv('DB_USER')
             db_password = os.getenv('DB_PASSWORD')
             db_name = os.getenv('DB_NAME')
 
-            db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@localhost:3306/{db_name}")
+            # db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@localhost:3306/{db_name}")
+
+
+
+            # Azure
+            db_user_azure = os.getenv('DB_USER_AZURE')
+            db_password_azure = os.getenv('DB_PASSWORD_AZURE')
+            db_server_name = os.getenv('DB_SERVER_NAME')
+            db_host = os.getenv('DB_HOST')
+            db_port = os.getenv("DB_PORT")
+            db_name_azure = os.getenv("DB_NAME_AZURE")
+            
+            db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user_azure}:{db_password_azure}@{db_host}:{db_port}/{db_name_azure}")
+
+            
             llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
             generate_query = create_sql_query_chain(llm, db)
 
